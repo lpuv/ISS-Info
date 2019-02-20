bb29c29b6ceed145423b1c8509119cacad1ff537
Gamer4Life Puvilland <uyenwontanswer@gmail.com>
2019-02-20 09:05:17 -0800

    Initial commit

---
+++ iss.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+import turtle
+import urllib.request
+
+url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+print('People in Space: ', result['number'])
+
+people = result['people']
+
+for p in people:
+    print(p["name"] + " is in the " + p["craft"])
+
+
+
